980dd1fe0554405497404a28e2446109c44b91af
RohiiitttPimpale <rohitpimpale2095@gmail.com>
2025-08-01 15:57:27 +0530

    initial commit with cleaning

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-# import numpy as np
-# from PIL import Image
-# import matplotlib.pyplot as plt
-#
-# red_array = np.array([255,0,0])
-# plt.imshow(red_array)
 import numpy as np
 from flask import Flask, render_template,url_for, redirect
 from flask_bootstrap import Bootstrap5
@@ -11,9 +5,7 @@ from flask_wtf import FlaskForm
 from wtforms import SubmitField,FileField
 from wtforms.validators import DataRequired
 from PIL import Image
-import matplotlib.pyplot as plt
 import os
-from collections import Counter
 
 class UploadForm(FlaskForm):
     image = FileField(label="Upload Image", validators=[DataRequired()])
@@ -23,17 +15,16 @@ class UploadForm(FlaskForm):
 app = Flask(__name__)
 bootstrap = Bootstrap5(app=app)
 
-app.secret_key = 'your_secret_key'
-app.secret_key = 'your_secret_key'  # Replace with a secure key
+app.secret_key = os.environ.get("FLASK_KEY")
 app.config['UPLOAD_FOLDER'] = 'static/image'
-app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024  # 2 MB limit
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
 
 @app.route("/",methods=["GET","POST"])
 def home():
     folder_path = "static/image"
     for file_name in os.listdir(folder_path):
         file_path = os.path.join(folder_path, file_name)
-        if os.path.isfile(file_path):  # Check if it's a file
+        if os.path.isfile(file_path):
             os.remove(file_path)
     my_form = UploadForm()
     if my_form.validate_on_submit():
@@ -44,11 +35,10 @@ def home():
 
 @app.route("/generator_colour_palette/<name>")
 def colour(name):
-    image = Image.open(f"static/image/{name}")  # Replace with your image path
-    image = image.convert("RGB")  # Ensure the image is in RGB format
+    image = Image.open(f"static/image/{name}")
+    image = image.convert("RGB")
     image_array = np.array(image)
 
-    # Reshape the image array to a 2D array of pixels
     pixels = image_array.reshape(-1, image_array.shape[-1])
     unique_colors, counts = np.unique(pixels, axis=0, return_counts=True)
     sort_id = np.flip(np.argsort(counts))
@@ -59,17 +49,6 @@ def colour(name):
         for ele in col:
             lis.append(int(ele))
         top_10.append(tuple(lis))
-
-
-
-    # # Get the 10 most common colors
-    # top_10_colors = color_counts.most_common(10)
-    # fix_10_colour = []
-    # for col in top_10_colors:
-    #     lis = []
-    #     for ele in col:
-    #         lis.append(int(ele))
-    #     fix_10_colour.append(tuple(lis))
 
     return render_template("colour.html",image=name,top_10=top_10)
 
